106996e236e098f87b2271c7d5b6708fbf088a07
oasjs <oasjatoba1@gmail.com>
2023-06-22 17:09:23 -0300

    Implementa o rolê

---
+++ script.js
@@ -0,0 +1,9 @@
+function textAreaAdjust(element) {
+  element.style.height = '1px';
+  element.style.height = (element.scrollHeight)+"px";
+}
+
+targets = document.querySelectorAll('textarea');
+  targets.forEach(element => {
+    element.style.height = "100px";
+  });
