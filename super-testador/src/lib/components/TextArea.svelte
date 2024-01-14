c49da319a3a0a8ac0a9a06ec5a7965c22250cf6e
oasjs <oasjatoba1@gmail.com>
2024-01-14 01:54:29 -0300

    Implement everything

---
+++ super-testador/src/lib/components/TextArea.svelte
@@ -0,0 +1,54 @@
+<script>
+	import { tick } from 'svelte';
+
+	export let fontSize;
+	export let lineHeight;
+	let value = '';
+
+	let textarea;
+	async function textAreaAdjust() {
+		textarea.style.height = 'auto';
+		await tick();
+		textarea.style.height = textarea.scrollHeight + 'px';
+
+		if (value.length > 0) {
+			textarea.style.padding = '0';
+		} else {
+			textarea.style.padding = '1rem';
+			textarea.style.height = '100px';
+		}
+	}
+</script>
+
+<textarea
+	id="textarea"
+	bind:value
+	bind:this={textarea}
+	on:input={textAreaAdjust}
+	placeholder="Digite aqui o texto que você quer testar."
+	spellcheck="false"
+	style="
+    font-size: {fontSize};
+    line-height: {lineHeight};
+  "
+></textarea>
+
+<style lang="scss">
+	textarea {
+		width: 100%;
+		height: 100px;
+		margin: 1rem 0;
+		padding: 1rem;
+
+		box-sizing: border-box;
+		border: none;
+		border-radius: 0.5rem;
+		outline: none;
+		resize: none;
+		overflow: hidden;
+
+		background-color: #f7f7f7;
+		text-align: justify;
+		hyphens: auto;
+	}
+</style>
--- super-testador/src/routes/+page.svelte
+++ super-testador/src/routes/+page.svelte
@@ -1,2 +1,66 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import TextArea from '$lib/components/TextArea.svelte';
+  let fontSize = '1rem';
+  let lineHeight = 'auto';
+</script>
+
+<main class="container">
+	<h1>Super Testador de Colunas (V2)</h1>
+	<p class="lead about">Bem vindo(a) ao <strong>Super Testador de Colunas!</strong></p>
+
+  <div class="spacer"></div>
+
+  <h4>
+    Configurações
+  </h4>
+  <div class="row">
+    <div class="col-md-6">
+      <div class="form-group">
+        <label for="fontSize">Tamanho da fonte</label>
+        <input type="text" class="form-control" id="fontSize" bind:value={fontSize} />
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="form-group">
+        <label for="lineHeight">Altura da linha</label>
+        <input type="text" class="form-control" id="lineHeight" bind:value={lineHeight} />
+      </div>
+    </div>
+  </div>
+
+  <div class="spacer"></div>
+
+  <h4>Testador</h4>
+	<div>
+		<TextArea fontSize={fontSize} lineHeight={lineHeight}></TextArea>
+		<div class="row">
+			<div class="col-md-6">
+				<TextArea fontSize={fontSize} lineHeight={lineHeight}></TextArea>
+			</div>
+			<div class="col-md-6">
+				<TextArea fontSize={fontSize} lineHeight={lineHeight}></TextArea>
+			</div>
+		</div>
+		<TextArea fontSize={fontSize} lineHeight={lineHeight}></TextArea>
+	</div>
+</main>
+
+<style lang="scss">
+	main {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+		height: 100vh;
+
+    h1 {
+      text-align: center;
+    }
+
+    .about {
+      text-align: center;
+    }
+
+    .spacer {
+      height: 3rem;
+    }
+	}
+</style>
